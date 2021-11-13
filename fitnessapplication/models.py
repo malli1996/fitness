35f171a82fb08b1b29a2d530e707a50c67f81855
mallikarjunaj <mallikarjuna.jangiti@adso.com>
2021-11-13 18:56:34 +0530

    model created

--- fitnessapplication/models.py
+++ fitnessapplication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    ename = models.CharField(max_length=20)
+    eadd = models.CharField(max_length=200)
+    
+
+
